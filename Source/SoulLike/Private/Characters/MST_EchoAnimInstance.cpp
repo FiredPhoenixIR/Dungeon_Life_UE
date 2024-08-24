679a1073e20742eae29a8bf736b8dd7d4baa6482
Mahdi ST <mahdi.sadeghian.tehrani@gmail.com>
2024-08-24 16:05:45 +0330

    AnimInstance / Animations

---
+++ Source/SoulLike/Private/Characters/MST_EchoAnimInstance.cpp
@@ -0,0 +1,25 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Characters/MST_EchoAnimInstance.h"
+#include "Characters/MST_EchoCharacter.h"
+#include "GameFramework/CharacterMovementComponent.h"
+#include "Kismet/KismetMathLibrary.h"
+
+void UMST_EchoAnimInstance::NativeInitializeAnimation()
+{
+	Super::NativeInitializeAnimation();
+
+	EchoCharacter = Cast<AMST_EchoCharacter>(TryGetPawnOwner());
+	if (EchoCharacter) {
+		EchoCharacterMovement = EchoCharacter->GetCharacterMovement();
+	}
+}
+
+void UMST_EchoAnimInstance::NativeUpdateAnimation(float DeltaSeconds)
+{
+	Super::NativeUpdateAnimation(DeltaSeconds);
+	if (EchoCharacterMovement) {
+		GroundSpeed = UKismetMathLibrary::VSizeXY(EchoCharacterMovement->Velocity);
+	}
+}
---
+++ Source/SoulLike/Public/Characters/MST_EchoAnimInstance.h
@@ -0,0 +1,32 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Animation/AnimInstance.h"
+#include "MST_EchoAnimInstance.generated.h"
+
+class AMST_EchoCharacter;
+class UCharacterMovementComponent;
+
+/**
+ Careful with null pointers as this is updating everty tick even when game is not running
+ **/
+UCLASS()
+class SOULLIKE_API UMST_EchoAnimInstance : public UAnimInstance
+{
+	GENERATED_BODY()
+	
+public:
+	virtual void NativeInitializeAnimation() override;
+	virtual void NativeUpdateAnimation(float DeltaSeconds) override;
+
+	UPROPERTY(BlueprintReadOnly) // Exposed to event graph
+	AMST_EchoCharacter* EchoCharacter;
+
+	UPROPERTY(BlueprintReadOnly, Category = "Movement")
+	UCharacterMovementComponent* EchoCharacterMovement;
+
+	UPROPERTY(BlueprintReadOnly, Category = "Movement")
+	float GroundSpeed;
+};
